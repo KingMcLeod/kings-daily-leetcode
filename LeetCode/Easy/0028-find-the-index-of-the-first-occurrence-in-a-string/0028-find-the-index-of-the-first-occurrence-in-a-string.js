f44172f2fba625cda379272ca2d149c81bcda86a
King M. <99514274+KingMcLeod@users.noreply.github.com>
2025-10-18 12:49:13 -0400

    Time: 0 ms (100%), Space: 54.4 MB (34.95%) - LeetHub

--- LeetCode/Easy/0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
+++ LeetCode/Easy/0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -14,7 +14,7 @@ D:
 
 A:
     1. initiate a loop to go through string
-        1a. create a second pointer (i + needle.length -1)
+        1a. create a second pointer (i + i + needle.length)
         1b. check to see if the current substring is the needle
             1ba. if it is, return i
             1bb. else, continue
@@ -29,7 +29,6 @@ A:
 var strStr = function(haystack, needle) {
     for (let i = 0; i < haystack.length; i++) {
         const currSubStr = haystack.substring(i, i + needle.length);
-        console.log("current substring we have: ", currSubStr);
         if (needle === currSubStr) return i;
     }
     return -1;
