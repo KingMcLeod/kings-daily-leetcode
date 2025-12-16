66dba617c4b745029b1f3637926e8ddb917b2e49
King M. <99514274+KingMcLeod@users.noreply.github.com>
2025-12-16 17:47:33 -0500

    Time: 19 ms (47%), Space: 56.4 MB (97.2%) - LeetHub

--- LeetCode/Easy/0383-ransom-note/0383-ransom-note.js
+++ LeetCode/Easy/0383-ransom-note/0383-ransom-note.js
@@ -2,24 +2,23 @@
 P:
     input: strings (ransomNote, magazine)
     output: boolean (true/false)
-    task: based on the magazine and ransomNote given, return true if the ransomNote can be created using letters from the magazine, or false otherwise
+    task: return true if ransomNote can be made out of magazine, and false otherwise
 
 E:
-    - strings will only contain lowercase english letters
-    - inputs will never be empty, guaranteed at least 1 character
-    - inputs can be different lengths
+    - both input strings will only consist of lowercase English letters
+    - inputs will never be empty, guaranteed at least 1 char
+    - order doesn't matter, as long as magazine has the letters
 
 D:
-    strings, booleans, hash tables
+    strings, booleans, map
 
 A:
-    1. create two empty hash tables for both inputs
-    2. iterate through each input and build out the hash table for them
-    3. use for in loop to iterate through the ransom note hash
-        3a. compare each key-value pair against the magazine hash
-            - if the value for that character is the same, continue
-            - if they aren't the same, return false immediately (can't be built out)
-    4. return true as base case (meaning we have gone through everything without any issues, can be built out)
+    1. creating 2 maps to keep track of letters in magazine and ransomNote
+    2. compare both maps against one another:
+        2a. check to see if magazine map has enough of a particular letter:
+            - if it doesn't, return false
+            - else continue
+    3. return true as base case 
 */
 
 /**
@@ -28,36 +27,36 @@ A:
  * @return {boolean}
  */
 var canConstruct = function(ransomNote, magazine) {
-    const noteHash = {};
-    const magHash = {};
+    const noteMap = mapBuilder(ransomNote);
+    const magMap = mapBuilder(magazine);
 
-    for (let i = 0; i < ransomNote.length; i++) {
-        const currentChar = ransomNote[i];
-        if (noteHash.hasOwnProperty(currentChar)) {
-            noteHash[currentChar]++;
-        } else {
-            noteHash[currentChar] = 1;
-        };
-    }
+    // console.log("note map: ", noteMap);
+    // console.log("magazine map: ", magMap);
 
-    for (let i = 0; i < magazine.length; i++) {
-        const currentChar = magazine[i];
-        if (magHash.hasOwnProperty(currentChar)) {
-            magHash[currentChar]++;
-        } else {
-            magHash[currentChar] = 1;
-        };
-    };
-
-    console.log(noteHash)
-    console.log(magHash)
-
-    for (const char in noteHash) {
-        if (noteHash[char] <= magHash[char] ) {
-            continue;
-        } else {
+    for (const char in noteMap) {
+        if (!magMap.hasOwnProperty(char)) {
             return false;
-        }
+        };
+
+        if (noteMap[char] > magMap[char]) {
+            return false;
+        };
     };
-    return true
+
+    return true;
 };
+
+const mapBuilder = (str) => {
+    const strMap = {};
+
+    for (let i = 0; i < str.length; i++) {
+        const currLetter = str[i];
+
+        if (!strMap[currLetter]) {
+            strMap[currLetter] = 1;
+        } else {
+            strMap[currLetter]++;
+        }
+    }
+    return strMap;
+}
