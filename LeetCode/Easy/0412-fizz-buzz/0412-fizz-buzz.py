69151b4e7599ca78f2dcb65c8430f39fa480ebf2
King M. <99514274+KingMcLeod@users.noreply.github.com>
2026-02-04 00:01:39 -0500

    Time: 1 ms (60.7%), Space: 13.6 MB (7.64%) - LeetHub

--- LeetCode/Easy/0412-fizz-buzz/0412-fizz-buzz.py
+++ LeetCode/Easy/0412-fizz-buzz/0412-fizz-buzz.py
@@ -1,15 +1,20 @@
 class Solution(object):
     def fizzBuzz(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
         output_list = []
 
         for i in range(1, n + 1):
-            if i % 3 == 0 and i % 5 == 0:  # Fixed: == 0
+            
+            if i % 3 == 0 and i % 5 == 0:
                 output_list.append("FizzBuzz")
-            elif i % 3 == 0:  # Fixed: == 0
+            elif i % 3 == 0:
                 output_list.append("Fizz")
-            elif i % 5 == 0:  # Fixed: == 0
+            elif i % 5 == 0:
                 output_list.append("Buzz")
             else:
-                output_list.append(str(i))  # Fixed: ) and str()
+                output_list.append(str(i))
 
         return output_list
