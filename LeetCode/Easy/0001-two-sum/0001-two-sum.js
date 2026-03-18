76eab4635179e84dd6be82283252208acb835856
King M. <99514274+KingMcLeod@users.noreply.github.com>
2026-03-18 11:26:13 -0400

    Time: 1 ms (87.16%), Space: 54.9 MB (52.76%) - LeetHub

--- LeetCode/Easy/0001-two-sum/0001-two-sum.js
+++ LeetCode/Easy/0001-two-sum/0001-two-sum.js
@@ -1,16 +1,43 @@
+/*
+P:
+    input: list of ints (nums) int (target)
+    output: tuple (indices of output nums)
+    task: find and return the indices (index positions) of two numbers that up to target within the nums input list 
+
+E: 
+    - cannot use the same element twice to reach target (unique index pos)
+    - return in any order
+    - input list will never be empty, guaranteed at least 2 nums minimum
+    - only one solution / valid answer to make target exists
+
+D:
+    ints, lists, dicts
+
+A:
+    1l. create empty object (hash map/table)
+    2. iterate through nums array
+        2a. store current num and compliment in variables
+        2b. check to see if compliment is already in our hash (has been seen already)
+            - if it has, return both index positions
+            - if not, store current num with index position in hash
+*/
+
 /**
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    const numsMap = {};
+    const numHash = {};
 
     for (let i = 0; i < nums.length; i++) {
-        const comp = target - nums[i];
+        const currentNum = nums[i];
+        const compliment = target - currentNum;
 
-        if (!numsMap.hasOwnProperty(comp)) {
-            numsMap[nums[i]] = i;
-        } else { return [i, numsMap[comp]]}
+        if (numHash.hasOwnProperty(compliment)) {
+            return [i, numHash[compliment]];
+        } else {
+            numHash[currentNum] = i;
+        }
     }
 };
