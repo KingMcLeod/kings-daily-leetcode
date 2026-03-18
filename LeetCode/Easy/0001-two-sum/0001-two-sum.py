1a82697605305d520cfa343acad1d202284a64b2
King M. <99514274+KingMcLeod@users.noreply.github.com>
2026-03-18 02:47:20 -0400

    Time: 1 ms (60.56%), Space: 20.5 MB (56.12%) - LeetHub

--- LeetCode/Easy/0001-two-sum/0001-two-sum.py
+++ LeetCode/Easy/0001-two-sum/0001-two-sum.py
@@ -1,12 +1,36 @@
+"""
+P:
+    input: list of ints (nums) int (target)
+    output: tuple (indices of output nums)
+    task: find and return the indices (index positions) of two numbers that up to target within the nums input list 
+
+E: 
+    - cannot use the same element twice to reach target (unique index pos)
+    - return in any order
+    - input list will never be empty, guaranteed at least 2 nums minimum
+    - only one solution / valid answer to make target exists
+
+D:
+    ints, lists, dicts
+
+A:
+    1l. create empty dict
+    2. iterate through nums list
+        2a. store current num and compliment in variables
+        2b. check to see if compliment is already in our dict (has been seen already)
+            - if it has, return both index positions
+            - if not, store current num with index position in dict
+"""
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        compliment_dict = {}
+        num_dict = {} # empty dictionary (key: num, val: index pos)
 
-        for i in range(len(nums)):
+        for i in range(len(nums)): # i to use get index pos
             num = nums[i]
-            compliment = target - num
+            compliment_num = target - num # calculate compliment we need
 
-            if compliment in compliment_dict:
-                return [i, compliment_dict[compliment]]
+            if compliment_num in num_dict: # if compliment is seen in our dict
+                return [i, num_dict[compliment_num]] # return both index pos (current number + compliment = target)
             else:
-                compliment_dict[num] = i
+                num_dict[num] = i # store it in the dict for going back to
