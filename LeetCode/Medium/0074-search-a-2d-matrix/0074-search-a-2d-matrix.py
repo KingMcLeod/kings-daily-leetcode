b264fcba06a2cd9446ddca7d275c481857c436f8
King M. <99514274+KingMcLeod@users.noreply.github.com>
2026-03-25 00:41:00 -0400

    Time: 0 ms (100%), Space: 19.7 MB (9.56%) - LeetHub

--- LeetCode/Medium/0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ LeetCode/Medium/0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -2,38 +2,38 @@
 P:
     input: 2-D array (matrix), int (target)
     output: boolean (True/False)
-    task: baesd on the input given, return true/false whether the target can be found in the matrix with a time complexity of O(log(m * n)) (make matrix size smaller to search)
+    output: given a 2D-array, scan it and return whether or not it was seen inside of it (True/False)
+    with a time complexity of O(log(m * n))
 
 E:
-    - input will never be empty, guaranteed at least 1 m * n (1x1) 
-    - each row is sorted (least to greatest)
-    - first integer in each row is greater than the last
+    - integers within matrix are in non-decreasing order (sorted)
+    - input will never be empty, guaranteed at least 1x1 matrix 
 
 D:
-    - ints, lists, matrix
+    int, arrays, pointers
 
-A: (sliding window approach?)
-    1. loop through matrix to access each row + cell
-        1a. check to see if the first int in row < target
-            - if it is, return False (means we've never found the target in our range)
-            - if it isn't: check to see if the current int == target:
-                - if it is, return True
+A:
+    1. create two pointers (start, end)
+    2. iterate through the matrix (rows + cols (cells))
+        2a. check if start + end pointer elements fall within target range (l <= target <= r)
+            - if it doesn't, return false immediately
+            - if it does, continue
+        2b. check to see if current value is target
+            - if it isn't, continue
+            - if it is, return true immediately (target found)
+    3. return false as base case (if iterated through entire matrix and target found)
 """
 
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        # for loop to access matrix
         for row in matrix:
-            first_int = row[0]
-            print("current row:" , row)
-            print("first integer:", first_int)
-
-            if first_int > target:
-                return False
-                
-            for col in row:
-                curr_cell = col
-                print("current cell:" , curr_cell) 
-
-                if curr_cell == target:
-                    return True
+            for col in range(len(row)):
+                # variables created
+                start, end = row[0], row[-1]
+                if start <= target and target <= end:
+                    if row[col] == target:
+                        return True
         return False
+
+        
