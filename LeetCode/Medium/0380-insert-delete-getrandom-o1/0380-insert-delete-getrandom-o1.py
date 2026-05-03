c7c7f3d43ff6b40a94c82d764e6b6a0bee056cd8
King M. <99514274+KingMcLeod@users.noreply.github.com>
2026-05-03 16:20:19 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Medium/0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
+++ LeetCode/Medium/0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
@@ -1,4 +1,4 @@
-import random, math
+import random
 
 class RandomizedSet:
 
@@ -21,27 +21,18 @@ class RandomizedSet:
         else:
             recent_val = self.arr[-1]
             val_index = self.hash_map[val]
-            recent_val_index = self.hash_map[recent_val]
 
             # index swap in array
-            self.arr[-1] = val
             self.arr[val_index] = recent_val
-
-            # index swap in hash map
             self.hash_map[recent_val] = val_index
-            self.hash_map[val] = recent_val_index
 
             # removal of value (O(1) time complexity)
             self.arr.pop()
             del self.hash_map[val]
-
             return True
         
     def getRandom(self) -> int:
-        random_index = random.randint(0,  len(self.arr) - 1)
-
-        return self.arr[random_index]
-        
+        return random.choice(self.arr)
 
 
 # Your RandomizedSet object will be instantiated and called as such:
