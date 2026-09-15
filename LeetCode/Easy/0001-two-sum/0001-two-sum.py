07e82697d5a7df30bba2cfe19c6e475fc14a70ec
King M. <99514274+KingMcLeod@users.noreply.github.com>
2026-09-15 19:36:56 -0400

    Time: 0 ms (100%), Space: 20.5 MB (41.98%) - LeetHub

--- LeetCode/Easy/0001-two-sum/0001-two-sum.py
+++ LeetCode/Easy/0001-two-sum/0001-two-sum.py
@@ -1,12 +1,12 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        num_dict = {}
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        num_dict = dict()
 
-        for i in range(len(nums)): 
-            curr_num = nums[i]
-            compliment = target - curr_num
-            
-            if compliment in num_dict:
-                return [i, num_dict[compliment]]
+        for i in range(len(nums)):
+            curr_num = nums[i] # store current num
+            compliment = target - curr_num # find compliment for pair
+
+            if compliment in num_dict: # if we've seen the second num alrady...
+                return [i, num_dict[compliment]] # then, return the index pos of both nums
             else:
-                num_dict[curr_num] = i
+                num_dict[curr_num] = i # else, store the index pos of current num
